3f23c139d9300c9e03a0863da50b7800068c1efa
theniswara <daniswara.mdr@proton.me>
2026-01-02 13:53:42 +0700

    feat: implement user initialization and storage in User class

--- taskia-free-html-master/js/User.js
+++ taskia-free-html-master/js/User.js
@@ -1,9 +1,21 @@
 class User {
+  constructor() {
+    this.users = this.getUsers() || [];
+  }
+
   saveUser(userData) {
     const newUser = {
       id: Date.now(),
       ...userData,
     };
-    console.log(userData);
+
+    this.users.push(newUser);
+    localStorage.setItem("users",JSON.stringify(this.users));
+
+    console.log("User berhasil disimpan:", newUser);
+  }
+
+  getUsers() {
+    return JSON.parse(localStorage.getItem("users") || "[]")
   }
 }
