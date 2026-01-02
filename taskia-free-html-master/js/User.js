4ea52f2c5e058c9740b05a3be3059dbf83d9d4ea
theniswara <daniswara.mdr@proton.me>
2026-01-02 13:59:09 +0700

    feat: enhance saveUser method with improved user storage and redirection

--- taskia-free-html-master/js/User.js
+++ taskia-free-html-master/js/User.js
@@ -9,9 +9,14 @@ class User {
       ...userData,
     };
 
+    // Simpan user baru ke array users
     this.users.push(newUser);
+
+    // Simpan ke localStorage
     localStorage.setItem("users",JSON.stringify(this.users));
 
+    // Arahkan ke halaman sigin
+    return (window.location.href = "../signin.html");
     console.log("User berhasil disimpan:", newUser);
   }
 
