366ae3a4c41925bfa86d6137fd1e8cbf924ea7fa
theniswara <daniswara.mdr@proton.me>
2026-01-04 19:20:42 +0700

    feat: Add sign-in logic to handle user authentication.

---
+++ taskia-free-html-master/js/signin.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userForm = document.getElementById("userForm");
+  const userManager = new User();
+
+  userForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const userNameByInput = document.getElementById("usernname").value;
+
+    const result = userManager.signIn(userNameByInput);
+
+    if (result.success) {
+      alert("success loggedin")
+      // return (window.location.href = "../tasks.html")
+    } else {
+      console.log("proses login gagal");
+    }
+  });
+});
